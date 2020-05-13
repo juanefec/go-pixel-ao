f61e408f9d1cdc443f33d4935ce4c07a53ea9e68
Lucas Amado <lucas.amadocordoba@gmail.com>
2020-05-13 17:16:16 -0300

    rename function

--- client/forest.go
+++ client/forest.go
@@ -47,7 +47,7 @@ type Forest struct {
 	Trees                                                          Trees
 }
 
-func generateRandomPoss(c, bot, top, left, rigth int64) []pixel.Vec {
+func generateRandomPosition(c, bot, top, left, rigth int64) []pixel.Vec {
 	poss := make([]pixel.Vec, c)
 	rand.Seed(c + top + bot + left + rigth)
 	for i := range poss {
@@ -143,15 +143,15 @@ func NewForest() *Forest {
 	}
 
 	// Fill outside
-	poss := generateRandomPoss(200, 0, int64(pathTop)-100, 0, 1800)
-	poss = append(poss, generateRandomPoss(200, 0, int64(pathTop)-100, 2200, 4000)...)
+	poss := generateRandomPosition(200, 0, int64(pathTop)-100, 0, 1800)
+	poss = append(poss, generateRandomPosition(200, 0, int64(pathTop)-100, 2200, 4000)...)
 	for i := range poss {
 		tree := pixel.NewSprite(treeSheet, treeFrames[rand.Intn(len(treeFrames))])
 		tree.Draw(treeBatch, pixel.IM.Scaled(pixel.ZV, 3.5).Moved(poss[i]))
 	}
 
 	// fill with zombie trees
-	zombieTrees := generateRandomPoss(5, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
+	zombieTrees := generateRandomPosition(5, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
 	ztree := trees[ZombieTree]
 	for i := 0; i <= len(zombieTrees)-1; i++ {
 		tree := pixel.NewSprite(*ztree.Pic, ztree.Frame)
@@ -161,14 +161,14 @@ func NewForest() *Forest {
 	// fill with tall trees
 	// commented because they were ugly
 
-	// tallTress := generateRandomPoss(10, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
+	// tallTress := generateRandomPosition(10, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
 	// ttree := trees[TallTree]
 	// for i := 0; i <= len(tallTress)-1; i++ {
 	// 	tree := pixel.NewSprite(*ttree.Pic, ttree.Frame)
 	// 	tree.Draw(ttree.Batch, pixel.IM.Scaled(pixel.ZV, 1.2).Moved(tallTress[i]))
 	// }
 
-	nltallTress := generateRandomPoss(8, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
+	nltallTress := generateRandomPosition(8, int64(pathTop)+100, int64(arenaTop), 1100, 2900)
 	nlttree := trees[NoLeafsTallTree]
 	for i := 0; i <= len(nltallTress)-1; i++ {
 		tree := pixel.NewSprite(*nlttree.Pic, nlttree.Frame)
